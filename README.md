ec583282db7f743727b9f97e7103fdc2bd74bb6b
Aaron Hedges <aaron@dashron.com>
2014-10-13 00:06:09 -0400

    pull filter out of roads

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ roads-fieldsfilter
 ==================
 
 A library for performing a specific form of API optimization. Check the readme for more details!
+
+ - [Roads.FieldsFilter](#roadsfieldsfilter)
+  - [new FieldsFilter(*dynamic* data)](#new-fieldsfilterdynamic-data)
+  - [filter(*Array* fields)](#filterarray-fields)
+
+
+
+  
---
+++ tests/testResponseFieldExpansion.js
@@ -0,0 +1,79 @@
+"use strict";
+
+var FieldsFilter = require('../lib/fieldsfilter');
+
+/**
+ * Test that single fields can be expanded properly
+ */
+exports.testExpandingSingleField = function (test) {
+	var filter = new FieldsFilter();
+	var expansion = filter._expandFields(['test']);
+
+	test.deepEqual({
+		'test' : true
+	}, expansion);
+	test.done();
+};
+
+/**
+ * Test that multipble fields can be expanded properly
+ */
+exports.testExpandingMultipleFields = function (test) {
+	var filter = new FieldsFilter();
+	var expansion = filter._expandFields(['test', 'stuff']);
+	
+	test.deepEqual({
+		'test' : true,
+		'stuff' : true
+	}, expansion);
+	test.done();
+};
+
+/**
+ * Test that nested fields are expanded properly
+ */
+exports.testExpandingSingleNestedField = function (test) {
+	var filter = new FieldsFilter();
+	var expansion = filter._expandFields(['test.one']);
+	
+	test.deepEqual({
+		'test' : {
+			'one' : true
+		}
+	}, expansion);
+	test.done();
+};
+
+/**
+ * Test that multiple nested fields are expanded properly
+ */
+exports.testExpandingMultipleNestedFields = function (test) {
+	var filter = new FieldsFilter();
+	var expansion = filter._expandFields(['test.one', 'hello.one']);
+	
+	test.deepEqual({
+		'test' : {
+			'one' : true
+		},
+		'hello' : {
+			'one' : true
+		}
+	}, expansion);
+	test.done();
+};
+
+/**
+ * Test that multiple nested fields with similar parents are expanded properly
+ */
+exports.testExpandingMultipleMatchingNestedFields = function (test) {
+	var filter = new FieldsFilter();
+	var expansion = filter._expandFields(['test.one', 'test.two']);
+	
+	test.deepEqual({
+		'test' : {
+			'one' : true,
+			'two' : true
+		}
+	}, expansion);
+	test.done();
+};
