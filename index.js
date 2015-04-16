6fb79b689f79e0eb254030ec29e7e2f871781d98
Aaron Hedges <aaron@dashron.com>
2015-04-16 10:15:51 -0400

    drop server and move to the middleware system for roads 3.0

--- index.js
+++ index.js
@@ -5,61 +5,18 @@ var url_module = require('url');
 var FieldsFilter = module.exports.FieldsFilter = require('./src/fieldsfilter.js');
 
 /**
- * Helper function to replace roads.api.server. This one automatically incorporates the fields filtering
- * 
- * @param  IncomingMessage http_request
- * @param  ServerResponse http_response
+ * Middleware for automatically applying the fields filter.
  */
-module.exports.server = function (http_request, http_response) {
-	var api = this;
-	var body = '';
-
-	var url = http_request.url;
-	var params = url_module.parse(url, true).query;
-	
-	if (typeof params.fields !== "undefined" && params.fields !== null) {
-		params.fields = params.fields.split(',');
-	} else {
-		params.fields = true;
-	}
-
-	http_request.on('readable', function () {
-  		var chunk;
-		while (null !== (chunk = http_request.read())) {
-			body += chunk;
-		}
-	});
-
-	var writeResponse = function (response) {
-		var filter = new module.exports.FieldsFilter(response.data);
-
-		filter.filter(params.fields)
-			.then(function (data) {
-				response.data = data;
-				
-				console.log(response.data);
-				// wrap up and write the response to the server
-				response.writeToServer(http_response);
-
-				// easy curl terminal readout
-				if (http_request.headers['user-agent'] && http_request.headers['user-agent'].indexOf('curl') !== -1) {
-					http_response.write("\n");
-				}
-
-				http_response.end();
+module.exports.middleware = function () {
+	return function (method, url, body, headers, next) {
+		return next()
+			.then(function (response) {
+				// Apply field filtering and value expansion on the response object
+				var filter = new FieldsFilter(response.body);
+				return filter.filter(url.query.fields).then(function (filtered_body) {
+					response.body = filtered_body;
+					return response;
+				});
 			});
 	};
-
-	http_request.on('end', function () {
-		// execute the api logic and retrieve the appropriate response object
-		api.request(http_request.method, http_request.url, body, http_request.headers)
-			.then(writeResponse);
-	});
-
-	// server request errors go to the unknown error representation
-	http_request.on('error', function (err) {
-		// todo: this all kinda sucks. It needs a refactor
-		console.log(err);
-		(new roads.Response({"error" : "An unknown error has occured"}, 500)).writeToServer(http_response);
-	});
 };
---
+++ tests/testMiddleware.js
@@ -0,0 +1,75 @@
+"use strict";
+
+var middleware = require('../index').middleware;
+
+/**
+ * Create a mock object to expand
+ */
+function testResponseObject (expand_func) {
+	var response = {
+		"name" : "aaron",
+		"description" : "developer",
+		"images" : [{
+			"url" : "http://www.dashron.com/image1.png",
+			"type" : "profile"
+		}, {
+			"url" : "http://www.dashron.com/image2.png",
+			"type" : "background"
+		}],
+		"active_image" : {
+			"url" : "http://www.dashron.com/image1.png",
+			"type" : "profile"
+		},
+		"delayed" : function () {
+			return "hello";
+		},
+		"delayed_obj" : function () {
+			return {
+				"test" : "yeah",
+				"huh" : "what"
+			};
+		},
+		"delayed_array" : function () {
+			return [{
+				"name" : "bob",
+			}, {
+				"name" : "tom"
+			}];
+		}
+	};
+
+	if (expand_func === true) {
+		response.delayed = response.delayed();
+		response.delayed_obj = response.delayed_obj();
+		response.delayed_array = response.delayed_array();
+	}
+
+	return response;
+}
+
+/**
+ * Test that a single key will filter out everything but that key
+ */
+exports.testAsFirstMiddleware = function (test) {
+	test.expect(1);
+
+	middleware()('GET', {
+			path : '/users',
+			query : {}
+		}, {}, {}, function () {
+		return new Promise(function (resolve, reject) {
+			// resolve with a fake response object
+			resolve({
+				body : testResponseObject()
+			});
+		});
+	})
+	.then(function (filtered_response) {
+		test.deepEqual(testResponseObject(true), filtered_response.body);
+		test.done();
+	})
+	.catch(function (err) {
+		console.log(err.stack);
+		test.fail();
+	});
+};//*/
